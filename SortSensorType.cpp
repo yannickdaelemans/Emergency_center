e378117a37e9dd0be5fbdcf8a5b791e682dcb117
LienAeyels <56587566+LienAeyels@users.noreply.github.com>
2019-10-27 02:56:08 +0200

    delete useless files

--- SortSensorType.cpp
+++
@@ -1,6 +0,0 @@
-#include "SortSensorType.h"
-
-SortSensorType::SortSensorType(Component* first, Component* second) {
-	// TODO - implement SortSensorType::SortSensorType
-	throw "Not yet implemented";
-}
